fdbdb18309709d3c41d3a628e4b89bf88f7d292a
Tago-0209 <ta020932@gmail.com>
2025-08-28 11:39:51 -0300

    aula 03

---
+++ Aula03/primeiro.java
@@ -0,0 +1,14 @@
+package Aula03;
+
+import java.util.Scanner;
+public class primeiro {
+public static void main(String[] args) {
+    String nome;
+    Scanner leitor = new Scanner(System.in);
+    System.out.println("Digite seu nome: ");
+    nome=leitor.nextLine();
+    System.out.println("Olá, "+nome+"!");
+    leitor.close();
+}
+}
+
